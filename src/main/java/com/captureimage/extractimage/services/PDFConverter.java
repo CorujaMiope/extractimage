084c34138329d502c2530ea2f8c76b150f568e88
Theodoro <124572914+CorujaMiope@users.noreply.github.com>
2025-01-23 23:47:37 -0300

    jar/path update

--- src/main/java/com/captureimage/extractimage/services/PDFConverter.java
+++ src/main/java/com/captureimage/extractimage/services/PDFConverter.java
@@ -3,33 +3,22 @@ package com.captureimage.extractimage.services;
 import com.captureimage.extractimage.dto.ImagePropertyDTO;
 import com.captureimage.extractimage.process.PDFEngine;
 import com.spire.doc.Document;
-import com.spire.doc.FileFormat;
 import com.spire.doc.PdfConformanceLevel;
 import com.spire.doc.ToPdfParameterList;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 public class PDFConverter {
 
+    private static final Logger log = LogManager.getLogger(PDFConverter.class);
     private static ToPdfParameterList parameter = new ToPdfParameterList();
-
-    private static Path path = Path.of("C:\\Users\\ADMIN\\TEMP_PDF");
-    private static File temp;
-
-    static {
-        try {
-            temp = File.createTempFile("temp", ".pdf", path.toFile());
-            //TODO criar uma função chamada "private String getPath()" que ira retornar todo esse escopo do path e file como caminho
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public static List<ImagePropertyDTO> docxToPDF(File file) throws IOException {
 
@@ -37,15 +26,35 @@ public class PDFConverter {
 
         parameter.setPdfConformanceLevel(PdfConformanceLevel.Pdf_A_1_A);
 
+        File temp = getFile();
+
+        if (temp != null) {
+            doc.saveToFile(temp.getAbsolutePath(), parameter);
+            PDFEngine engine = new PDFEngine(PDDocument.load(temp));
+            return engine.getImagePropertyDTOs();
+        }
+
+        return List.of();
+    }
+
+    private static File getFile() {
+        //TODO criar uma função chamada "private String getPath()" que ira retornar todo esse escopo do path e file como caminho
+
+        Path path = Path.of("C:\\TEMP_PDF");
+
         if (!path.toFile().exists()) {
             path.toFile().mkdirs();
         }
 
-        doc.saveToFile(temp.getAbsolutePath(), parameter);
+        try {
+            File temp = File.createTempFile("temp", ".pdf", path.toFile());
+            return temp;
 
-        PDFEngine engine = new PDFEngine(PDDocument.load(temp));
+        } catch (IOException e) {
+            log.error("Error because: ", e);
+        }
 
-        return engine.getImagePropertyDTOs();
+        return null;
     }
 
     public static List<ImagePropertyDTO> docxToPDF(byte[] bytes) throws IOException {
@@ -53,13 +62,14 @@ public class PDFConverter {
         Document doc = new Document(new ByteArrayInputStream(bytes));
         parameter.setPdfConformanceLevel(PdfConformanceLevel.Pdf_A_1_A);
 
-        if (!path.toFile().exists()) {
-            path.toFile().mkdirs();
+        File temp = getFile();
+
+        if (temp != null) {
+            doc.saveToFile(temp.getAbsolutePath(), parameter);
+            PDFEngine engine = new PDFEngine(PDDocument.load(temp));
+            return engine.getImagePropertyDTOs();
         }
 
-        doc.saveToFile(temp.getAbsolutePath(), parameter);
-
-        PDFEngine engine = new PDFEngine(PDDocument.load(temp));
-        return engine.getImagePropertyDTOs();
+        return List.of();
     }
 }
